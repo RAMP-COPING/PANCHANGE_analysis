b73e1f364630b79394a356edf59a75cf3d3df452
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2020-12-16 20:28:05 +0000

    Added keyworker status to employment variable

--- figures/Paper_detail_checks.R
+++ figures/Paper_detail_checks.R
@@ -5,6 +5,35 @@
 
 # Read in data
 library(tidyverse)
+library(data.table)
+library(lubridate)
 
-source(./)
+# get file info
+source("/Users/kirstin/Dropbox/SGDP/RAMP/Projects/PANCHANGE/PANCHANGE_raw_path.R")
+
+
+# check the date of the last COPING baseline data point.
+
+# read in the 4 cohort data
+
+alldat <-  readRDS(file = paste0(data_path, "/four_cohorts.rds"))
+rampdat <-  readRDS(file = paste0(data_path, "/ramp.rds"))
+
+## Subset samples
+
+GLAD <- alldat %>%
+  filter(Sample == "GLAD" | 
+           Sample == "EDGI")
+
+NBR <- alldat %>%
+  filter(Sample == "NBR")
+
+RAMP <- alldat %>%
+  filter(Sample == "RAMP")
+
+## Get the most recent baseline sign up per sample
+
+max(RAMP$startDate.coping)
+max(GLAD$startDate.coping)
+max(NBR$startDate.coping)
 
