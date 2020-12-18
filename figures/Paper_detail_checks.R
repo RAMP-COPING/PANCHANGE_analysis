77ceaf8b797b3c95bdaa1dea00d69728cd42574d
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2020-12-18 14:02:51 +0000

    added t-test by group for retrospective

--- figures/Paper_detail_checks.R
+++ figures/Paper_detail_checks.R
@@ -42,4 +42,21 @@ t.test(alldat$gad.sum_score_base,alldat$gad.sum_score_prepan, paired = T)
 t.test(alldat$phq.sum_score_base,alldat$phq.sum_score_prepan, paired = T)
 
 t.test(alldat$gad.sum_score_base,alldat$gad.sum_score_retro, paired = T)
-t.test(alldat$phq.sum_score_base,alldat$phq.sum_score_retro, paired = T)
+unlist(t.test(alldat$phq.sum_score_base,alldat$phq.sum_score_retro, paired = T)$estimate)
+
+
+## T-tests for average scores by group (retrospective)
+
+alldat %>%
+  mutate(sample = case_when(Sample == "GLAD" | Sample == "EDGI" ~ "GLAD",
+                            Sample == "RAMP" ~ "RAMP",
+                            Sample == "NBR" ~ "NBR")) %>%
+  select(gad.sum_score_base,gad.sum_score_retro, sample) %>%
+  gather(key = variable, value = value, -sample) %>% 
+  group_by(sample,variable) %>% 
+  summarise(value = list(value)) %>%
+  spread(variable,value) %>%
+  group_by(sample) %>% 
+  mutate(p_value = t.test(unlist(gad.sum_score_base), unlist(gad.sum_score_retro))$p.value,
+         t_value = t.test(unlist(gad.sum_score_base), unlist(gad.sum_score_retro))$statistic,
+         df = t.test(unlist(gad.sum_score_base), unlist(gad.sum_score_retro))$parameter)
