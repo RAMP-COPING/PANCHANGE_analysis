86991ce5cb353062e2c8799f6677748d1a052185
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2021-01-07 15:06:50 +0000

    ensured comaprable sample filtering for detail checks

--- figures/Paper_detail_checks.R
+++ figures/Paper_detail_checks.R
@@ -17,32 +17,87 @@ source("/Users/kirstin/Dropbox/SGDP/RAMP/Projects/PANCHANGE/PANCHANGE_raw_path.R
 # read in the 4 cohort data
 
 alldat <-  readRDS(file = paste0(data_path, "/four_cohorts_variables_exclusion.rds"))
-## Subset samples
 
-GLAD <- alldat %>%
+# impute retro for those who did not change
+
+alldat$gad.sum_score_retro <- ifelse(alldat$gad.pandemic_felt_feelings == "No different", alldat$gad.sum_score_base,alldat$gad.sum_score_retro)
+alldat$phq.sum_score_retro <- ifelse(alldat$phq.pandemic_felt_feelings == "No different", alldat$phq.sum_score_base,alldat$phq.sum_score_retro)
+
+## subsample for retro-baseline analysis
+### exclude those who do not have complete responses for both baseline and retrospective
+### in addition to those already excluded for NA in age, gender and MH diagnosis.
+
+
+dat.retro <- alldat %>%
+  filter(
+    ((data_group_gad == "Baseline and retrospective data" |
+        data_group_gad == "All prepandemic, baseline and retrospective data") |
+       (data_group_phq == "Baseline and retrospective data" |
+          data_group_phq == "All prepandemic, baseline and retrospective data") |
+       (data_group_pcl == "Baseline and retrospective data" |
+          data_group_pcl == "All prepandemic, baseline and retrospective data"))
+  )
+
+
+## subsample for prepan-baseline analysis
+
+### exclude those who do not have complete responses for both baseline and prepandemic
+### in addition to those already excluded for NA in age, gender and MH diagnosis.
+
+dat.prepan <- alldat %>%
+  filter(
+    ((data_group_gad == "Prepandemic and baseline data" |
+        data_group_gad == "All prepandemic, baseline and retrospective data") |
+       (data_group_phq == "Prepandemic and baseline data" |
+          data_group_phq == "All prepandemic, baseline and retrospective data") |
+       (data_group_pcl == "Prepandemic and baseline data" |
+          data_group_pcl == "All prepandemic, baseline and retrospective data") ) &
+      prepandemic_march_1_numeric == 1 # Exclude participants that signed up after the 1. March
+  )
+
+dim(dat.prepan)
+
+
+## Subset samples by study
+
+GLAD.prepan <- dat.prepan %>%
   filter(Sample == "GLAD" | 
            Sample == "EDGI")
 
-NBR <- alldat %>%
+NBR.prepan <- dat.prepan %>%
   filter(Sample == "NBR")
 
-RAMP <- alldat %>%
+RAMP.prepan <- dat.prepan %>%
+  filter(Sample == "RAMP")
+
+### retro
+GLAD.retro <- dat.retro %>%
+  filter(Sample == "GLAD" | 
+           Sample == "EDGI")
+
+NBR.retro <- dat.retro %>%
+  filter(Sample == "NBR")
+
+RAMP.retro <- dat.retro %>%
   filter(Sample == "RAMP")
 
 ## Get the most recent baseline sign up per sample
 
-max(RAMP$startDate.coping)
-max(GLAD$startDate.coping)
-max(NBR$startDate.coping)
+max(RAMP.prepan$startDate.coping)
+max(GLAD.prepan$startDate.coping)
+max(NBR.prepan$startDate.coping)
 
-## T-tests for average scores
+## T-tests for average scores: Prepandemic
 
+t.test(dat.prepan$gad.sum_score_base,dat.prepan$gad.sum_score_prepan, paired = T)
+t.test(dat.prepan$phq.sum_score_base,dat.prepan$phq.sum_score_prepan, paired = T)
+t.test(dat.prepan$pcl.sum_score_base,dat.prepan$pcl.sum_score_prepan, paired = T)
 
-t.test(alldat$gad.sum_score_base,alldat$gad.sum_score_prepan, paired = T)
-t.test(alldat$phq.sum_score_base,alldat$phq.sum_score_prepan, paired = T)
+## T-tests for average scores: Retrospective
 
-t.test(alldat$gad.sum_score_base,alldat$gad.sum_score_retro, paired = T)
-unlist(t.test(alldat$phq.sum_score_base,alldat$phq.sum_score_retro, paired = T)$estimate)
+t.test(dat.retro$gad.sum_score_base,dat.retro$gad.sum_score_retro, paired = T)
+t.test(dat.retro$phq.sum_score_base,dat.retro$phq.sum_score_retro, paired = T)
+
 
 
 ## T-tests for average scores by group (retrospective)
