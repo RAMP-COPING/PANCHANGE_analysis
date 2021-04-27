6abc0f495008ff4268266bd4eb54b087cf4a9af3
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2021-04-27 14:57:12 +0100

    knitted new version with updated forest plots

--- FunctionLib/lm_forest.R
+++ FunctionLib/lm_forest.R
@@ -1,17 +1,20 @@
 # Function to create forest plots with different grouping sets using formatted data
 # KLPurves
 
-lm.forest <- function(data,title,pointsize=10,linesize=3,errorsize=3,axistext=60,axistitle=80,legtitle=70,stripsize=50,colourpal=COPINGpalette5){
+lm.forest <- function(data,title,pointsize=10,linesize=3,errorsize=3,
+                      axistext=60,axistitle=80,legtitle=70,stripsize=50,
+                      colourpal=COPINGpalette5,lowlim=-3,uplim=4,dodge=0.5)
   
+  {
+
   p = ggplot(data=data,
              aes(x =RowLabels,y = Estimate,
                  colour = Group)) +
     
     geom_point(size = pointsize,
-               position = position_dodge(0.9)) +
+               position = position_dodge(dodge)) +
     
-    geom_hline(aes(fill=Group),
-               yintercept =0,
+    geom_hline(yintercept =0,
                linetype=2,
                size = linesize)  +
     
@@ -22,15 +25,16 @@ lm.forest <- function(data,title,pointsize=10,linesize=3,errorsize=3,axistext=60
     geom_errorbar(aes(ymin = LowerBound,
                       ymax = UpperBound),
                   width = 1,
-                  position = position_dodge(0.9),
+                  position = position_dodge(dodge),
                   size = errorsize)  +
     
     #facet_wrap(. ~ Categories,
     #           strip.position="left",nrow=6, scales="free_y") +
     
-    facet_grid(. ~ Categories, strip.position="left", drop = TRUE) +
+    facet_grid(. ~ Categories,  drop = TRUE) +
     
     scale_colour_manual(values=colourpal) +
+    scale_y_continuous(limits=c(lowlim,uplim)) +
     
     theme(plot.title=element_blank(), #element_text(size=0,face="bold"),
           axis.text=element_text(face="bold",size=axistext,),
